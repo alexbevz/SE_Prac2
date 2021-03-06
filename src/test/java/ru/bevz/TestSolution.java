5309c645dcd0edef79ef40f0518be8eb09d41609
Aleksandr Bevz <as-bivz@yandex.ru>
2021-03-06 14:25:57 +0300

    Add new test for Solution.maxArea() and add dependency org.junit.jupiter

--- src/test/java/ru/bevz/TestSolution.java
+++ src/test/java/ru/bevz/TestSolution.java
@@ -1,19 +1,31 @@
 package ru.bevz;
 
-import org.junit.Assert;
 import org.junit.Test;
-import ru.bevz.Solution;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-public class TestSolution {
+import java.util.stream.Stream;
 
-//  Пробовал сделать по примеру из лекции, но возникли трудности с определением индекса текущего теста
-//	@ParameterizedTest
-//	@ValueSource(strings = {
-//					"[1,8,6,2,5,4,8,3,7]",
-//					"[1,1]",
-//					"[4,3,2,1,4]",
-//					"[1,2,1]",
-//	})
+class TestSolution {
+
+
+	@ParameterizedTest
+	@MethodSource("provideArraysData")
+	void TestMaxArea(int[] values, int expected) {
+		Solution solution = new Solution();
+		Assertions.assertEquals(expected, solution.maxArea(values));
+	}
+
+	private static Stream<Arguments> provideArraysData() {
+		return Stream.of(
+						Arguments.of(new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7,}, 49),
+						Arguments.of(new int[]{1, 1,}, 1),
+						Arguments.of(new int[]{4, 3, 2, 1, 4,}, 16),
+						Arguments.of(new int[]{1, 2, 1,}, 2)
+		);
+	}
 
 	@Test
 	public void TestMaxArea() {
@@ -30,6 +42,6 @@ public class TestSolution {
 		for (int i = 0; i < expecteds.length; i++) {
 			actuals[i] = solution.maxArea(values[i]);
 		}
-		Assert.assertArrayEquals(expecteds, actuals);
+		Assertions.assertArrayEquals(expecteds, actuals);
 	}
 }
